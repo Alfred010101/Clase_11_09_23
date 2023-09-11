4bd76de557011e8aa11f4c130fa6984192022a0f
Alfredo <arista21dominguez@gmail.com>
2023-09-11 11:47:35 -0600

    Insertar/Eliminar

--- src/oop/TestMatrizDinamica.java
+++ src/oop/TestMatrizDinamica.java
@@ -11,10 +11,16 @@ public class TestMatrizDinamica
         MatrizDinamica objMatrizD = new MatrizDinamica();
 
         objMatrizD.insertar(19);
+        objMatrizD.insertar(221);
+        objMatrizD.insertar(1);
+        objMatrizD.insertar(34);
+        objMatrizD.insertar(23);
         
         System.out.println(objMatrizD.desplegar());
-        objMatrizD.eliminar(0);
+        objMatrizD.eliminar(4);
         System.out.println(objMatrizD.desplegar());
+        //objMatrizD.eliminar(0);
+        //System.out.println(objMatrizD.desplegar());
         //System.out.println(objMatrizD.getArr().length);
     }    
 }
